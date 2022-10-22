b159dda1d9a74678900ccbddca7d3fe45084dfef
pavanmu8 <93365371+pavanmu8@users.noreply.github.com>
2022-10-22 12:41:28 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,16 @@
+arr = [25, 11, 7, 75, 56];     
+     
+
+max = arr[0];    
+     
+  
+for i in range(0, len(arr)):    
+  
+   if(arr[i] > max):    
+       max = arr[i];    
+           
+print("Largest element present in given array: " + str(max));  
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
